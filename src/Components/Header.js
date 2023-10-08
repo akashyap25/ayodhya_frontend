5b5a7534adc6aa0afc36e4b5c2a07b59936aab1c
akashyap25 <anuragkashyap026@gmail.com>
2023-10-08 12:29:14 +0530

    header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,46 +1,51 @@
 import React, { useState, useEffect } from "react";
 import logo from "../Assets/logo.png";
 
-const slogans = [
-  "कण कण में विष्णु बसें जन जन में श्रीराम,\n प्राणों में माँ जानकी मन में बसे हनुमान",
-  "हनुमान हैं नाम महान, हनुमान करे बेड़ा पार,\n जो जपता हैं नाम हनुमान, होते सब दिन एक समान।",
-  "राम जिनका नाम है, अयोध्या जिनका धाम है,\n ऐसे रघुनंदन को, हमारा प्रणाम है."
-];
-
 const Header = () => {
-  const [currentSloganIndex, setCurrentSloganIndex] = useState(0);
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setCurrentSloganIndex((prevIndex) =>
-        (prevIndex + 1) % slogans.length
-      );
-    }, 10000); // Change slogan every 10 seconds
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
-
   return (
-    <header className="flex flex-col mb-6 lg:mb-8 w-full ">
-      <div className="flex w-full h-14 justify-center bg-orange-500"></div>
-      <div className="flex items-center">
-        <img
-          src={logo}
-          alt=""
-          className="lg:mr-4 ml-8 max-w-full h-auto lg:w-auto"
-          style={{ maxWidth: "150px" }}
-        />
-        <span>
-          <h1 className="text-2xl font-bold text-orange-500">
-            प्रसाद आपके द्वार
-          </h1>
-          <h3 className="ml-2 font-semibold text-sm">prasadaapkedwaar.in</h3>
-        </span>
-        <p className="hidden lg:block ml-80 mt-4 text-lg font-bold italic text-slate-700" style={{ whiteSpace: "pre-line" }}>
-          {slogans[currentSloganIndex]}
+    <header className="flex flex-col mb-6 lg:mb-8 w-full">
+      <div className="flex w-full h-8 lg:h-8 items-center font-semibold text-base justify-center bg-orange-500 overflow-hidden">
+        <p className="marquee text-white">
+         || ॐ नमः शिवाय || हनुमान जी || प्रभु श्री राम || राधा रानी ||
         </p>
+      </div>
+      <div className="flex flex-col items-center lg:flex-row justify-between lg:items-center">
+        <div className="flex items-center w-full gap-6 lg:w-auto">
+          <img
+            src={logo}
+            alt=""
+            className="lg:mr-4 max-w-full h-auto lg:w-auto"
+            style={{ maxWidth: "150px" }}
+          />
+          <span>
+            <h1 className="text-2xl font-bold text-orange-500">
+              प्रसाद आपके द्वार
+            </h1>
+            <h3 className="ml-2 font-semibold text-sm">prasadaapkedwaar.in</h3>
+          </span>
+        </div>
+        <div className="flex items-center mx-auto gap-4 lg:gap-16">
+          <button
+            type="submit"
+            className="bg-orange-500 text-white font-semibold text-xl py-2 px-4 lg:px-8 lg:py-4 rounded-lg hover:bg-orange-600 transition duration-200 mb-2 lg:mb-0"
+          >
+            Ayodhya
+          </button>
+
+          <button
+            type="submit"
+            className="bg-orange-700 text-white font-semibold text-xl py-2 px-4 lg:px-8 lg:py-4 rounded-lg hover:bg-orange-500 transition duration-200 mb-2 lg:mb-0"
+          >
+            Mathura
+          </button>
+
+          <button
+            type="submit"
+            className="bg-orange-700 text-white font-semibold text-xl py-2 px-4 lg:px-8 lg:py-4 rounded-lg hover:bg-orange-500 transition duration-200 mb-2 lg:mb-0"
+          >
+            Varanasi
+          </button>
+        </div>
       </div>
     </header>
   );
